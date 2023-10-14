fcf896dddfbd330116705b757bc15a316c2c1e0e
J_AFF <86849624+J4FF@users.noreply.github.com>
2023-10-14 23:28:23 +0200

    Delete remote desktop aktivieren test.ps1

--- remote desktop aktivieren test.ps1
+++
@@ -1,5 +0,0 @@
-# Aktiviere Remote-Desktop
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
-
-# Starte den Remote-Desktop-Dienst neu
-Restart-Service -Name TermService
